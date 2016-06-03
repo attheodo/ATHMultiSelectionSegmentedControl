b4fe0161dfc2188c0dedd44e433971cb792b43d2
attheodo <at@atworks.gr>
2016-06-03 16:07:31 +0300

    bid bada boom

---
+++ Example/Pods/Target Support Files/ATHMultiSelectionSegmentedControl/ATHMultiSelectionSegmentedControl-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ATHMultiSelectionSegmentedControl : NSObject
+@end
+@implementation PodsDummy_ATHMultiSelectionSegmentedControl
+@end
---
+++ Example/Pods/Target Support Files/ATHMultiSelectionSegmentedControl/ATHMultiSelectionSegmentedControl-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double ATHMultiSelectionSegmentedControlVersionNumber;
+FOUNDATION_EXPORT const unsigned char ATHMultiSelectionSegmentedControlVersionString[];
+
