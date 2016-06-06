a3445c2cfbea772b20f77c0d5b57cce7ff943077
attheodo <at@atworks.gr>
2016-06-06 20:30:51 +0300

    feat: basic subclass logic

--- ATHMultiSelectionSegmentedControl/Classes/ATHMultiSelectionControlSegmentButton.swift
+++ ATHMultiSelectionSegmentedControl/Classes/ATHMultiSelectionControlSegmentButton.swift
@@ -7,3 +7,63 @@
 //
 
 import Foundation
+import UIKit
+
+internal class ATHMultiSelectionControlSegmentButton: UIButton {
+    
+    // MARK: - Private Properties
+    private var _isButtonSelected: Bool = false
+    
+    // MARL: - Public Properties
+    public var isButtonSelected: Bool {
+        return _isButtonSelected
+    }
+    
+    // MARK: - Initialisers
+    public init() {
+        
+        super.init(frame: CGRectZero)
+        _configureButton()
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    // MARK: - Private Methods
+    private func _configureButton() {
+
+        backgroundColor = UIColor.clearColor()
+        
+        setTitleColor(tintColor, forState: .Normal)
+        
+        layer.borderWidth = 0.5
+        layer.borderColor = tintColor.CGColor
+        
+        showsTouchWhenHighlighted = true
+
+    }
+    
+    private func _setSelectedState() {
+        
+        backgroundColor = tintColor
+        setTitleColor(UIColor.whiteColor(), forState: .Normal)
+        
+    }
+    
+    private func _setUnselectedState() {
+        
+        backgroundColor = UIColor.clearColor()
+        setTitleColor(tintColor, forState: .Normal)
+        
+    }
+    
+    // MARK: - Public Methods
+    public func setButtonSelected(isSelected: Bool) {
+        
+        _isButtonSelected = isSelected
+        _isButtonSelected ? _setSelectedState() : _setUnselectedState()
+        
+    }
+}
