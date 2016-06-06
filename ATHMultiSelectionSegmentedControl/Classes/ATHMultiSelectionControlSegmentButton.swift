ecf89d16b3f0a038824bba03e84e997a0f14c524
attheodo <at@atworks.gr>
2016-06-06 20:18:06 +0300

    chore: renamed file

---
+++ ATHMultiSelectionSegmentedControl/Classes/ATHMultiSelectionControlSegmentButton.swift
@@ -0,0 +1,9 @@
+//
+//  ATHMultiSelectionControlSegmentButton.swift
+//  Pods
+//
+//  Created by Athanasios Theodoridis on 06/06/16.
+//
+//
+
+import Foundation
---
+++ ATHMultiSelectionSegmentedControl/Classes/ATHMultiSelectionSegmentedControl.swift
@@ -0,0 +1,9 @@
+//
+//  ATHMultiSelectionSegmentedControl.swift
+//  Pods
+//
+//  Created by Athanasios Theodoridis on 06/06/16.
+//
+//
+
+import Foundation
