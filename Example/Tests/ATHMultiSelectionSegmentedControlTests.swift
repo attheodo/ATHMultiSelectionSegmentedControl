cf283f61d76ebae0753117811a90bec9b20c8f8c
attheodo <at@atworks.gr>
2016-06-07 20:48:57 +0300

    improve: more tests

--- Example/Tests/ATHMultiSelectionSegmentedControlTests.swift
+++ Example/Tests/ATHMultiSelectionSegmentedControlTests.swift
@@ -32,4 +32,26 @@ class ATHMultiSelectionSegmentedControlTests: XCTestCase {
         expect(self.segmentedControl.numberOfSegments) == 4
     
     }
+    
+    func testSelectedSegmentIndicesPropertyWithNoItems() {
+        expect(self.segmentedControl.selectedSegmentIndices.count) == 0
+    }
+    
+    func testSelectedSegmentIndicesPropertyWithItemsButNoSelection() {
+        
+        segmentedControl.insertSegmentsWithTitles(["1", "2", "3", "4"])
+        segmentedControl.layoutSubviews()
+        
+        expect(self.segmentedControl.selectedSegmentIndices.count) == 0
+
+    }
+    
+    func testSelectedSegmentIndicesPropertyWithItemsAndSelection() {
+        
+        segmentedControl.insertSegmentsWithTitles(["1", "2", "3", "4"])
+        segmentedControl.layoutSubviews()
+        
+        segmentedControl.selectedSegmentIndices = [0, 1]
+
+    }
 }
