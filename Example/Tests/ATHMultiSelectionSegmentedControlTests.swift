46e3ef5db2c8dddf97bd020369f022f5dbe06d27
attheodo <at@atworks.gr>
2016-06-08 16:20:56 +0300

    test: added more tests

--- Example/Tests/ATHMultiSelectionSegmentedControlTests.swift
+++ Example/Tests/ATHMultiSelectionSegmentedControlTests.swift
@@ -52,6 +52,77 @@ class ATHMultiSelectionSegmentedControlTests: XCTestCase {
         segmentedControl.layoutSubviews()
         
         segmentedControl.selectedSegmentIndices = [0, 1]
+        
+        expect(self.segmentedControl.selectedSegmentIndices.count) == 2
 
+    }
+    
+    func testTitleForSegmentAtIndexMethodWithInvalidIndex() {
+        
+        segmentedControl.insertSegmentsWithTitles(["1", "2", "3", "4"])
+        segmentedControl.layoutSubviews()
+
+        expect(self.segmentedControl.titleForSegmentAtIndex(-1)).to(beNil())
+        
+    }
+    
+    func testTitleForSegmentAtIndexMethod() {
+        
+        segmentedControl.insertSegmentsWithTitles(["1", "2", "3", "4"])
+        segmentedControl.layoutSubviews()
+        
+        expect(self.segmentedControl.titleForSegmentAtIndex(0)) == "1"
+        expect(self.segmentedControl.titleForSegmentAtIndex(1)) == "2"
+        expect(self.segmentedControl.titleForSegmentAtIndex(2)) == "3"
+        expect(self.segmentedControl.titleForSegmentAtIndex(3)) == "4"
+        expect(self.segmentedControl.titleForSegmentAtIndex(100)) == "4"
+    
+    }
+    
+    func testSetTitleForSegmentAtIndexMethod() {
+        
+        segmentedControl.insertSegmentsWithTitles(["1", "2", "3", "4"])
+        segmentedControl.layoutSubviews()
+
+        segmentedControl.setTitle("one", forSegmentAtIndex: 0)
+        segmentedControl.setTitle("two", forSegmentAtIndex: 1)
+        segmentedControl.setTitle("three", forSegmentAtIndex: 2)
+        segmentedControl.setTitle("four", forSegmentAtIndex: 3)
+        
+        expect(self.segmentedControl.titleForSegmentAtIndex(0)) == "one"
+        expect(self.segmentedControl.titleForSegmentAtIndex(1)) == "two"
+        expect(self.segmentedControl.titleForSegmentAtIndex(2)) == "three"
+        expect(self.segmentedControl.titleForSegmentAtIndex(3)) == "four"
+        
+        segmentedControl.setTitle("five", forSegmentAtIndex: 100)
+        expect(self.segmentedControl.titleForSegmentAtIndex(3)) == "five"
+        
+    }
+    
+    func testInsertSegmentAtIndexMethodWithNegativeIndex() {
+        
+        segmentedControl.insertSegmentWithTitle("blah", atIndex: -1, animated: true)
+        
+        expect(self.segmentedControl.numberOfSegments) == 0
+        
+    }
+    
+    func testInsertSegmentAtIndexMethod() {
+        
+        segmentedControl.insertSegmentWithTitle("one", atIndex: 0, animated: true)
+
+        expect(self.segmentedControl.numberOfSegments) == 1
+        expect(self.segmentedControl.titleForSegmentAtIndex(0)) == "one"
+        
+        segmentedControl.insertSegmentWithTitle("two", atIndex: 0, animated: true)
+        expect(self.segmentedControl.numberOfSegments) == 2
+        expect(self.segmentedControl.titleForSegmentAtIndex(0)) == "two"
+        
+        segmentedControl.insertSegmentWithTitle("three", atIndex: 1, animated: true)
+        expect(self.segmentedControl.numberOfSegments) == 3
+        expect(self.segmentedControl.titleForSegmentAtIndex(0)) == "two"
+        expect(self.segmentedControl.titleForSegmentAtIndex(1)) == "three"
+        expect(self.segmentedControl.titleForSegmentAtIndex(2)) == "one"
+        
     }
 }
