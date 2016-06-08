19865637c3da4f5e6d1dd1c6e2520d0b411edc36
attheodo <at@atworks.gr>
2016-06-08 20:13:20 +0300

    initial example project

--- Example/ATHMultiSelectionSegmentedControl/ViewController.swift
+++ Example/ATHMultiSelectionSegmentedControl/ViewController.swift
@@ -7,12 +7,23 @@
 //
 
 import UIKit
+import ATHMultiSelectionSegmentedControl
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var multiSegmentedControl: MultiSelectionSegmentedControl!
+    @IBOutlet weak var segmented: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        multiSegmentedControl.insertSegmentsWithTitles(["one", "two", "three"])
+
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(1 * Double(NSEC_PER_SEC))), dispatch_get_main_queue()) { () -> Void in
+            //self.segmented.insertSegmentWithTitle("lala", atIndex: 2, animated: false)
+            //self.multiSegmentedControl.removeSegmentAtIndex(2, animated: true)
+            self.multiSegmentedControl.insertSegmentWithTitle("four", atIndex: 4, animated: true)
+        }
     }
 
     override func didReceiveMemoryWarning() {
