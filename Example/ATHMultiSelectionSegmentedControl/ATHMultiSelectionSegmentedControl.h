e49a02a685c3e90a13ef52c0de8b6554b0f1691d
attheodo <at@atworks.gr>
2016-11-13 14:53:39 +0200

    add carthage support

---
+++ Example/ATHMultiSelectionSegmentedControl/ATHMultiSelectionSegmentedControl.h
@@ -0,0 +1,19 @@
+//
+//  ATHMultiSelectionSegmentedControl.h
+//  ATHMultiSelectionSegmentedControl
+//
+//  Created by Athanasios Theodoridis on 13/11/2016.
+//  Copyright © 2016 CocoaPods. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ATHMultiSelectionSegmentedControl.
+FOUNDATION_EXPORT double ATHMultiSelectionSegmentedControlVersionNumber;
+
+//! Project version string for ATHMultiSelectionSegmentedControl.
+FOUNDATION_EXPORT const unsigned char ATHMultiSelectionSegmentedControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ATHMultiSelectionSegmentedControl/PublicHeader.h>
+
+
